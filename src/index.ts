a5f847e7e0d67485e58de4bdac8abc50be7ccbbd
Ramirez Monje Lucia <lrammon392@g.educaand.es>
2021-09-28 09:47:32 +0200

    first commit

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { leerTeclado } from './util/entradaTeclado'
 console.log("Hola mundo")
 
 let main = async () => {
-        const valor = await leerTeclado('Dime algo')
+        const valor = await leerTeclado('¿Qué edad tienes?')
         let edad: number = parseInt(valor)
         if (edad >= 18) {
                 console.log(`Tienes ${edad} años, eres mayor de edad`)
